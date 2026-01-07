fa5e0b7a0360a4f5e3d44a4300f203d403e79368
drkdhong <drkdhong@gmail.com>
2026-01-07 23:14:40 +0900

    app.py is modified by kdhong

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    # 개발자 1, 2가 기능을 추가할 공간입니다.
-    return '<h1>Hello Team! Ready for CI/CD!</h1>'
+    # 개발자 1이 추가한 코드:
+    user_name = "Developer 1"
+    return f'<h1>Hello Team! Ready for CI/CD!</h1><p>Welcome, {user_name}!</p>'
 
 # Render.com에서 실행할 때 필요한 코드 (필수는 아님)
 if __name__ == '__main__':
     app.run(debug=True)
+
