46d26c56baf4826a93673e4f830d8d23d749a686
drkdhong <drkdhong@gmail.com>
2026-01-08 11:21:10 +0900

    drkdhong is added

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     # 개발자 1이 추가한 코드:
-    user_name = "Developer 1"
+    user_name = "Developer drkdhong"
     return f'<h1>Hello Team! Ready for CI/CD!</h1><p>Welcome, {user_name}!</p>'
 
 # Render.com에서 실행할 때 필요한 코드 (필수는 아님)
